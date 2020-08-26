f8d8ae05b872327e0cf2d75ea2cfd3db0ed7c053
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-08-26 17:15:39 +0200

    Add shell script get questions from a given condition id

---
+++ scripts/get_questions.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+CONDITION_ID=$1
+
+if [ $# -ne 1 ];
+then
+  echo "usage: $1 CONDITION_ID_HASH"
+  exit -1
+fi
+
+if [ -n $CONDITION_ID ]; then
+
+HASH_ID=`curl -s 'https://api.thegraph.com/subgraphs/name/cag/hg' \
+ -H 'content-type: application/json' \
+ --data-binary '{"query":"{ conditions(where : { id: \"'${CONDITION_ID}'\"}) { id oracle questionId }}"}' | jq '.data["conditions"][].questionId' | cut -d'"' -f 2`
+ 
+ if [ -n $HASH_ID ]; then
+  echo "questionID: $HASH_ID";
+  json=`curl -s 'https://api.thegraph.com/subgraphs/name/protofire/omen' \
+    -H 'content-type: application/json' \
+    -d '{"query":"{  questions(  where: {    id: \"'${HASH_ID}'\"  }  ) {    id    templateId    data    title  }}"}'`    
+  echo $json | jq '.data["questions"][].data'
+ fi
+
+fi
