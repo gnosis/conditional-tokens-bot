4673bef20b161202504c9de07adfcea7f8f347d4
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-10-15 14:19:25 +0200

    Refactor conditions and questions queries
    BREAKING CHANGE: Replace graph environment variable endpoints to the
    following:
      - `THE_GRAPH_CONDITIONAL_TOKENS`: Env var for Conditional Tokens
        subgraphs.
      - `THE_GRAPH_OMEN`: Env var for Omen subgraphs.
    
    Remove `indexedFixedProductMarketMakers` from getQuestion to add the
    queried entity into `getCondition` service. Remove the `creator` query
    from getConditions (obtained from the transaction id). Add a check for
    errors on the queries and throw Error in case `json.errors` exist.

--- src/services/getCondition.js
+++ src/services/getCondition.js
@@ -2,24 +2,29 @@ const fetch = require('node-fetch');
 const _ = require("lodash/collection");
 
 module.exports.getCondition = (conditionId) => {
-  const jsonQuery = { query: `{  conditions(  where: {    id: \"${conditionId}\"  }  ) { id creator oracle questionId }}` }
+  const jsonQuery = { query: `{  conditions(  where: {    id: \"${conditionId}\"  }  ) { id oracle questionId fixedProductMarketMakers { id outcomeTokenMarginalPrices scaledLiquidityParameter } }}` }
 
-  const promise = fetch(process.env.THE_GRAPH_GET_OMEN_CONDITIONS, {
+  const promise = fetch(process.env.THE_GRAPH_OMEN, {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(jsonQuery),
     method: "POST",
   }).then(res => res.json())
   .catch(error => console.error('Error:', error))
-  .then(json => {    
-
+  .then(json => {
     const conditions = new Array();
-    _.forEach(json.data.conditions, condition => {
-      conditions.push({
-        questionId: condition.questionId,
-        creator: condition.creator
+    if(!json.errors && json.data.conditions) {
+      _.forEach(json.data.conditions, condition => {
+        conditions.push({
+          oracle: condition.oracle,
+          questionId: condition.questionId,
+          fixedProductMarketMakers: (condition.fixedProductMarketMakers.length > 0) ? condition.fixedProductMarketMakers[0].id : null,
+          outcomeTokenMarginalPrices: (condition.fixedProductMarketMakers.length > 0) ? condition.fixedProductMarketMakers[0].outcomeTokenMarginalPrices : null,
+          scaledLiquidityParameter: (condition.fixedProductMarketMakers.length > 0) ? condition.fixedProductMarketMakers[0].scaledLiquidityParameter : null,
+        });
       });
-    });
-
+    } else {
+      throw new Error(json.errors[0].message);
+    }
     return conditions;
   });
 
--- src/services/getQuestion.js
+++ src/services/getQuestion.js
@@ -2,9 +2,9 @@ const fetch = require('node-fetch');
 const _ = require("lodash/collection");
 
 module.exports.getQuestion = (questionId) => {
-  const jsonQuery = { query: `{  questions(  where: {    id: \"${questionId}\"  }  ) { id templateId indexedFixedProductMarketMakers { id outcomeTokenMarginalPrices scaledLiquidityParameter } outcomes title category }}` }
+  const jsonQuery = { query: `{  questions(  where: {    id: \"${questionId}\"  }  ) { id templateId outcomes title category }}` }
 
-  const promise = fetch(process.env.THE_GRAPH_GET_OMEN_QUESTIONS, {
+  const promise = fetch(process.env.THE_GRAPH_OMEN, {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(jsonQuery),
     method: "POST",
@@ -17,9 +17,6 @@ module.exports.getQuestion = (questionId) => {
         title: question.title,
         outcomes: question.outcomes,
         category: question.category,
-        indexedFixedProductMarketMakers: question.indexedFixedProductMarketMakers[0].id,
-        outcomeTokenMarginalPrices: question.indexedFixedProductMarketMakers[0].outcomeTokenMarginalPrices,
-        scaledLiquidityParameter: question.indexedFixedProductMarketMakers[0].scaledLiquidityParameter,
       })
     });
     return questions;
