8a72e18fd8a03c567761c09dd6576d442a3363dc
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-10-15 14:50:30 +0200

    Add getTrade event and refactor get new market
    Get trades from a scheduled job for every `JOB_GET_TRADE_MINUTES` minutes:
     - Add `node-schedule` package to create job.
     - Import `tradeEvents` to look for new trades from a given creation timestamp
     and format the message to be pushed.
    
    Refactor "New market created" messages:
     - Import truncate string utils to format addresses.
     - Replace question `indexFixedProductMarketMakers` field to `condition.fixedProductMarketMakers`.
     - Remove the event console.log.
    
    Remove `conditionalTokenContract` and its events.
    
    Resolves: #12

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-require('dotenv').config()
-const Web3 = require('web3');
 const _ = require("lodash/collection");
 const express = require("express");
-const { IncomingWebhook } = require('@slack/webhook');
+const schedule = require('node-schedule');
+
+const { urlExplorer, webhook, web3 } = require('./config/constants');
+const { truncate } = require('./utils/utils');
 const { getconditionalTokensContract, getFixedProductionMarketMakerFactoryContract } = require('./services/contractEvents');
 const { getTokenName, getTokenSymbol, getTokenDecimals } = require('./services/contractERC20');
 const { getQuestion } = require('./services/getQuestion');
 const { getCondition } = require('./services/getCondition');
+const { tradeEvents } = require('./events/tradeEvents');
 
 const app = express();
 const port = 3000;
-const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ETH_NODE));
-const conditionalTokenContract = getconditionalTokensContract(web3);
-const fixedProductMarketMakerContract = getFixedProductionMarketMakerFactoryContract(web3);
+const fixedProductMarketMakerFactoryContract = getFixedProductionMarketMakerFactoryContract(web3);
 
 app.use(express.json());
 
@@ -21,41 +21,39 @@ app.get('/', (req, res) => {
 })
 app.listen(port, () => console.log(`Bot listening on port ${port}!`));
 
-// Read a url from the environment variables
-const url = process.env.SLACK_WEBHOOK_URL;
-const networkName = process.env.NETWORK_NAME;
-const urlExplorer = networkName == "rinkeby" ? 'rinkeby.etherscan.io' : 'etherscan.io';
-
-// Initialize webhook for Slack
-const webhook = url ? new IncomingWebhook(url) : undefined;
-if (webhook === undefined) {
-    console.log('SLACK_WEBHOOK_URL not defined!!');
-}
+// Look for trade events every minute
+console.log(`Configure get trades job for every ${process.env.JOB_GET_TRADE_MINUTES} minutes`);
+const timestamp = Math.floor(Date.now() / 1000);
+console.log(`Looking for new trades at ${timestamp}`);
+tradeEvents(web3, webhook, urlExplorer, timestamp, process.env.JOB_GET_TRADE_MINUTES*60);
+schedule.scheduleJob(`*/${process.env.JOB_GET_TRADE_MINUTES} * * * *`, function(){
+    const timestamp = Math.floor(Date.now() / 1000);
+    console.log(`Looking for new trades at ${timestamp}`);
+    tradeEvents(web3, webhook, urlExplorer, timestamp, process.env.JOB_GET_TRADE_MINUTES*60);
+});
 
 /**
  * Watching FixedProductMarketMakerCreation
  */
-fixedProductMarketMakerContract.events.FixedProductMarketMakerCreation({
+fixedProductMarketMakerFactoryContract.events.FixedProductMarketMakerCreation({
     filter: {},
     fromBlock: process.env.START_BLOCK,
 }, (error, event) => {
-    console.log(event); // same results as the optional callback above
-    // Send the notification
     (async () => {
-        const message = new Array('> *New market created!* :tada:\n> ');
+        const message = new Array('<!here>', '*New market created!* :tada:');
         //TODO support conditional markets
         getCondition(event.returnValues.conditionIds[0]).then((conditions) => {
             _.forEach(conditions, condition => {
                 getQuestion(condition.questionId).then((questions) => {
                     if (questions.length == 0) {
-                        console.error(`ERROR: Question for hex "${condition.questionId}" not found on contition ID ${event.returnValues.conditionId}`);
+                        console.error(`ERROR: Question for hex "${condition.questionId}" not found on contition ID ${event.returnValues.conditionIds[0]}`);
                     } else {
                         _.forEach(questions, question => {
-                            message.push(`> *Title:*\n> ${question.title}`,
+                            message.push(`> *<https://omen.eth.link/#/${condition.fixedProductMarketMakers}|${question.title}>*`,
                                 `> *Outcomes:*`);
-                            if (question.outcomeTokenMarginalPrices) {
+                            if (condition.outcomeTokenMarginalPrices) {
                                 _.forEach(question.outcomes, (outcome, i) => {
-                                    message.push(`> \`${(parseFloat(question.outcomeTokenMarginalPrices[i]) * 100 ).toFixed(2)}%\` - ${outcome}`);
+                                    message.push(`> \`${(parseFloat(condition.outcomeTokenMarginalPrices[i]) * 100 ).toFixed(2)}%\` - ${outcome}`);
                                 });
                             } else {
                                 _.forEach(question.outcomes, (outcome, i) => {
@@ -68,10 +66,9 @@ fixedProductMarketMakerContract.events.FixedProductMarketMakerCreation({
                                 getTokenDecimals(web3, event.returnValues.collateralToken).then(decimals => {
                                     web3.eth.getTransaction(event.transactionHash).then(transaction => {
                                         message.push(`> *Collateral*: <https://${urlExplorer}/token/${event.returnValues.collateralToken}|${tokenName}>`,
-                                            `> *Liquidity*: ${parseFloat(questions[0].scaledLiquidityParameter).toFixed(2)} ${tokenSymbol}`,
-                                            `> *Omen URL*: <https://omen.eth.link/#/${questions[0].indexedFixedProductMarketMakers}|--&gt;>`,
-                                            `> *Created by*: <https://${urlExplorer}/address/${transaction.from}|${transaction.from}>`);
-                                        webhook ? webhook.send({
+                                            `> *Liquidity*: ${parseFloat(condition.scaledLiquidityParameter).toFixed(2)} ${tokenSymbol}`,
+                                            `> *Created by*: <https://${urlExplorer}/address/${transaction.from}|${truncate(transaction.from, 14)}>`);
+                                        webhook && webhook.send({
                                             blocks: [{
                                                 type: 'section',
                                                 text: {
@@ -79,15 +76,9 @@ fixedProductMarketMakerContract.events.FixedProductMarketMakerCreation({
                                                     text: message.join('\n')
                                                     }
                                                 }
-                                                ,{
-                                                type: 'section',
-                                                text: {
-                                                    type: 'mrkdwn',
-                                                    text: `<!here>`,
-                                                    }
-                                                }
                                             ]
-                                        }) : console.log(event.returnValues.conditionIds[0] + ':\n' + message + '\n\n\n');
+                                        });
+                                        console.log(event.returnValues.conditionIds[0] + ':\n' + message + '\n');
                                     });
                                 });
                             });
@@ -98,286 +89,3 @@ fixedProductMarketMakerContract.events.FixedProductMarketMakerCreation({
         });
     })();
 });
-
-/**
- * Split Positions
- */
-conditionalTokenContract.events.PositionSplit({
-    filter: {},
-    fromBlock: process.env.START_BLOCK
-}, function(error, event){
-    // Could be a new generic created market or a new trade
-    console.log(event);
-})
-.on('data', function(event){
-    console.log(event); // same results as the optional callback above
-})
-.on('changed', function(event){
-    // remove event from local database
-})
-.on('error', console.error);
-
-
-// watching create condition
-conditionalTokenContract.events.ConditionPreparation({
-    filter: {}, 
-    fromBlock: process.env.START_BLOCK
-}, function(error, event){ 
-    console.log(event);
-})
-.on('data', function(event){
-    console.log(event); // same results as the optional callback above
-})
-.on('changed', function(event){
-    // remove event from local database
-})
-.on('error', console.error);
-
-// watching condition resolution
-conditionalTokenContract.events.ConditionResolution({
-    filter: {}, 
-    fromBlock: process.env.START_BLOCK
-}, function(error, event){ 
-    console.log(event); 
-    // Send the notification
-    (async () => {
-        webhook && await webhook.send({
-        blocks: [
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `>----`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `-> *New \`${event.event}\` at block <https://${urlExplorer}/block/${event.blockNumber}|${event.blockNumber}>*`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*conditionId*: ${event.returnValues.conditionId}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*oracle*: <https://${urlExplorer}/address/${event.returnValues.oracle}|${event.returnValues.oracle}>`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*questionId*: ${event.returnValues.questionId}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*outcomeSlotCount*: ${event.returnValues.outcomeSlotCount}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*payoutNumerators*: ${event.returnValues.payoutNumerators}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `>----`,
-                }
-            }],
-        });
-    })();
-})
-.on('data', function(event){
-    console.log(event); // same results as the optional callback above
-})
-.on('changed', function(event){
-    // remove event from local database
-})
-.on('error', console.error);
-
-/**
- * Merge Positions
- */
-conditionalTokenContract.events.PositionsMerge({
-    filter: {}, 
-    fromBlock: process.env.START_BLOCK
-}, function(error, event){ 
-    console.log(event); 
-    // Send the notification
-    (async () => {
-        webhook && await webhook.send({
-        blocks: [
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `>----`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `-> *New \`${event.event}\` at block <https://${urlExplorer}/block/${event.blockNumber}|${event.blockNumber}>*`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*stakeholder*: ${event.returnValues.stakeholder}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*collateralToken*: <https://${urlExplorer}/address/${event.returnValues.collateralToken}|${event.returnValues.collateralToken}>`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*parentCollectionId*: ${event.returnValues.parentCollectionId}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*conditionId*: ${event.returnValues.conditionId}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*partition*: ${event.returnValues.partition}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*amount*: ${event.returnValues.amount}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `>----`,
-                }
-            }],
-        });
-    })();
-})
-.on('data', function(event){
-    console.log(event); // same results as the optional callback above
-})
-.on('changed', function(event){
-    // remove event from local database
-})
-.on('error', console.error);
-
-/**
- * Payout Redemption
- */
-conditionalTokenContract.events.PayoutRedemption({
-    filter: {}, 
-    fromBlock: process.env.START_BLOCK
-}, function(error, event){ 
-    console.log(event); 
-    // Send the notification
-    (async () => {
-        webhook && await webhook.send({
-        blocks: [
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `>----`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `-> *New \`${event.event}\` at block <https://${urlExplorer}/block/${event.blockNumber}|${event.blockNumber}>*`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*redeemer*: ${event.returnValues.redeemer}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*collateralToken*: <https://${urlExplorer}/address/${event.returnValues.collateralToken}|${event.returnValues.collateralToken}>`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*parentCollectionId*: ${event.returnValues.parentCollectionId}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*conditionId*: ${event.returnValues.conditionId}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*indexSets*: ${event.returnValues.indexSets}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `*payout*: ${event.returnValues.payout}`,
-                }
-            },
-            {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `>----`,
-                }
-            }],
-        });
-    })();
-})
-.on('data', function(event){
-    console.log(event); // same results as the optional callback above
-})
-.on('changed', function(event){
-    // remove event from local database
-})
-.on('error', console.error);
