a8d9364a838bcd0efb8259357b09a538f781cc59
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-10-15 13:49:11 +0200

    Refactor module to config and utils file
     - Export `urlExplorer`, `webhook`, and `web3` from configuration `config`
     path to import on main or other modularized files.
     - Add utils file with formatting text utils like `truncate`.

---
+++ src/utils/utils.js
@@ -0,0 +1,18 @@
+const truncate = (fullStr, strLen, separator) => {
+    if (fullStr.length <= strLen) return fullStr;
+
+    separator = separator || '...';
+
+    var sepLen = separator.length,
+        charsToShow = strLen - sepLen,
+        frontChars = Math.ceil(charsToShow/2),
+        backChars = Math.floor(charsToShow/2);
+
+    return fullStr.substr(0, frontChars) + 
+           separator + 
+           fullStr.substr(fullStr.length - backChars);
+};
+
+module.exports = {
+    truncate,
+}
