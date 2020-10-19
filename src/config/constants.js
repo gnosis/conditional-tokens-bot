084b823cc235402850662191d0eea2723ae4df9c
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-10-19 18:23:46 +0200

    Add check web3 connection on event `end`

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,6 +8,12 @@ const urlExplorer = networkName == "rinkeby" ? 'rinkeby.etherscan.io' : 'ethersc
 // Create a web3 instance
 const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ETH_NODE));
 
+// Check websocket connection event
+web3._provider.on('end', (eventObj) => {
+    console.error('The websocket was Disconnected');
+});
+
+
 module.exports = {
     slackUrl,
     urlExplorer,
