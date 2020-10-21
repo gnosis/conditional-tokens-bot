089c885e8dde8d41c070589531e9f1cf4d2eec32
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-10-21 13:40:52 +0200

    Add web3 and `getLastBlockNumber` to new Web3 util
    Move web3 instance from config/constants.js file to new web3 utils file
    in the `src/utils` path.
    Add async method `getLastBlockNumber` to `./utils/web3.js` file.

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,21 +1,10 @@
 require('dotenv').config()
-const Web3 = require('web3');
 
 const slackUrl = process.env.SLACK_WEBHOOK_URL;
 const networkName = process.env.NETWORK_NAME;
 const urlExplorer = networkName == "rinkeby" ? 'rinkeby.etherscan.io' : 'etherscan.io';
 
-// Create a web3 instance
-const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ETH_NODE));
-
-// Check websocket connection event
-web3._provider.on('end', (eventObj) => {
-    console.error('The websocket was Disconnected');
-});
-
-
 module.exports = {
     slackUrl,
     urlExplorer,
-    web3,
 }
