fb6df4de27a0c573043e6191b9f82a426d9aab46
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-10-26 13:49:28 +0100

    Replace Web3 WebsocketProvider to HttpProvider
    Replace the websocket connecntion due to the replace of get current
    events to call `getPastEvents` method from specific `fromBlock` to
    `toBlock` values.

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,12 +1,7 @@
 const Web3 = require('web3');
 
 // Create a web3 instance
-const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ETH_NODE));
-
-// Check websocket connection event
-web3._provider.on('end', (eventObj) => {
-    console.error('The websocket was Disconnected');
-});
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_NODE));
 
 const getLastBlockNumber = async () => {
     return web3.eth.getBlockNumber();
