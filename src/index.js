b6b7786bb76816f8bfcc3b87380f8a872380cc98
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-10-27 19:29:33 +0100

    Add watch event for market is resolved
    - Add `getResolvedMarketsEvents` method to get event from Conditional
    Tokens smart contract when `ConditionResolution` is emitted.
    - Rename method `watchFPMMCreationEvent` to `getFPMMCreationEvent` and
    exported method `watchNewMarketsEvent` to `watchCreationMarketsEvent`.
    - Remove then from Promise.all on `getFPMMCreationEvent`.
    
    Resolves: #32

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const schedule = require('node-schedule');
 const packageJson = require('../package.json');
 const { port, jobTime } = require('./config');
 const { pushSlackMessage } = require('./utils/slack');
-const { watchNewMarketsEvent } = require('./events/marketEvents');
+const { watchCreationMarketsEvent, watchResolvedMarketsEvent } = require('./events/marketEvents');
 const { findTradeEvents } = require('./events/tradeEvents');
 const { findLiquidityEvents } = require('./events/liquidityEvents');
 
@@ -24,13 +24,15 @@ const startMessage = `Conditional Tokens bot \`${version}\` was started.`;
 pushSlackMessage(startMessage);
 console.log(startMessage);
 
-// Watch new market created events
+// Watch new market created and resolved market events
 let lastUsedBlock = 0;
 schedule.scheduleJob(`*/${jobTime} * * * *`, function() {
     const fromBlock = lastUsedBlock ? lastUsedBlock : 0;
-    watchNewMarketsEvent(fromBlock).then(toBlock => {
+    watchCreationMarketsEvent(fromBlock).then(toBlock => {
         lastUsedBlock = toBlock;
     });
+    // Watch resolved markets
+    watchResolvedMarketsEvent(fromBlock);
 });
 
 // Look for trade events every minute
